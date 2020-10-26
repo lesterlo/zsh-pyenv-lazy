5b03ada348829e82044b01eedf6b31e53f7c6eaf
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2020-10-26 13:32:20 +0800

    Add shell name to pyenv init command

--- pyenv-lazy.plugin.zsh
+++ pyenv-lazy.plugin.zsh
@@ -9,9 +9,9 @@ if type pyenv > /dev/null; then
     export PATH="${PYENV_ROOT}/bin:${PYENV_ROOT}/shims:${PATH}"
     function pyenv() {
         unset -f pyenv
-        eval "$(command pyenv init -)"
+        eval "$(command pyenv init - zsh)"
         if [[ -n "${ZSH_PYENV_LAZY_VIRTUALENV}" ]]; then
-            eval "$(command pyenv virtualenv-init -)"
+            eval "$(command pyenv virtualenv-init - zsh)"
         fi
         pyenv $@
     }
